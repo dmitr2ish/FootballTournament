f53ffb857c8d30e263a523b90ebe0679668ea842
dmitr2ish <dmitr2ish@yandex.ru>
2020-07-27 16:24:30 +0500

    - init

---
+++ src/main/java/com/example/footballTournament/FootballTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballTournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 	FootballTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTournamentApplication.class, args);
+	}
+
+}
