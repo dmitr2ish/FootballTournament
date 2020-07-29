2a76fc542459f4858c9f9916f62599d4aa39efbd
dmitr2ish <dmitr2ish@yandex.ru>
2020-07-29 08:56:46 +0500

    - added main view by MaV of MainController

--- src/main/java/com/example/footballTournament/controller/view/MainController.java
+++ src/main/java/com/example/footballTournament/controller/view/MainController.java
@@ -1,4 +1,4 @@
-package com.example.footballTournament.controller;
+package com.example.footballTournament.controller.view;
 
 import com.example.footballTournament.entity.Game;
 import com.example.footballTournament.entity.Group;
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,10 +43,8 @@ public class MainController {
     }
 
     @GetMapping("/")
-    public String getMainPage(ModelMap modelMap) {
-        List<Group> groupList = groupService.getAllGroups();
-        modelMap.addAttribute("groupList", groupList);
-        return "main";
+    public ModelAndView getMainPage() {
+        return new ModelAndView("main");
     }
 
 }
