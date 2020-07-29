cc15a3f723f249356eba150e426050e4eb6a6e85
dmitr2ish <dmitr2ish@yandex.ru>
2020-07-29 09:06:04 +0500

    - added rest method get group list

---
+++ src/main/java/com/example/footballTournament/controller/rest/GroupRestController.java
@@ -0,0 +1,27 @@
+package com.example.footballTournament.controller.rest;
+
+import com.example.footballTournament.entity.Group;
+import com.example.footballTournament.service.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/group")
+public class GroupRestController {
+
+    final private GroupService service;
+
+    @Autowired
+    public GroupRestController(GroupService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/list")
+    public List<Group> getListGroup() {
+        return service.getAllGroups();
+    }
+}
